9dd6dcda3745fd9773ee070b1ac7f15e6cf639b5
Manuel Cristóbal Escobedo <manuelcristobalescobedo@gmail.com>
2025-11-19 21:28:34 -0300

    Cambio en el enlace de la aPI para los componentes Producto.tsx y Principal.tsx

--- src/Paginas/Producto.tsx
+++ src/Paginas/Producto.tsx
@@ -47,7 +47,7 @@ const Producto: React.FC = () => {
     useEffect(() => {
         async function getProducto() {
             try {
-                const response = await axios.get(`http://13.218.227.241:9461/productos/${id}`);
+                const response = await axios.get(`http://18.215.170.96:9461/productos/${id}`);
                 const productoMapeado = mapearProductoBackend(response.data);
                 setProducto(productoMapeado);
             } catch (error) {
